5e71f7c7770abfd74a8b34c18302d03cce0e01d2
derry derajat <derryderajat@mail.com>
2022-12-15 14:04:18 +0700

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const mongoose = require('mongoose');
 const app = express();
 mongoose.set('strictQuery', true);
 var corsOptions = {
-  origin: 'https://tofservice.vercel.app/',
+  origin: 'https://tof.herokuapp.com/',
 };
 
 app.use(cors(corsOptions));
